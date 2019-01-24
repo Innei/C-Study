78946e1dc3c4a6099c1c57955b85c458fbc99b9f
yiny0liuyin <yi00oi9hhg@qq.com>
2019-01-24 16:31:05 +0800

    refeash

--- 数据结构/队列queue.c
+++ 数据结构/队列queue.c
@@ -34,14 +34,63 @@ bool en_queue(QUEUE *pQ, int val)
 }
 void traverse_queue(QUEUE *pQ)
 {
-    
-}
+    int i = pQ->front;
 
+    while (i != pQ->rear)
+    {
+        printf("%d ", pQ->pBase[i]);
+        i = (i + 1) % 6;
+    }
+    putchar('\n');
+    return;
+}
+bool empty_queue(QUEUE *pQ)
+{
+
+    if (pQ->front == pQ->rear)
+    {
+        return true;
+    }
+    return false;
+}
+bool out_queue(QUEUE *pQ, int *pVal)
+{
+    
+
+    if (empty_queue(pQ))
+    {
+        return false;
+    }
+    *pVal = pQ->pBase[pQ->front];
+   
+    pQ->front = (pQ->front + 1) % 6;
+    
+    return true;
+}
 int main(int argc, char const *argv[])
 {
     QUEUE Q;
-
+    int Val;
     init(&Q);
+    en_queue(&Q, 1);
+    en_queue(&Q, 2);
+    en_queue(&Q, 3);
+    en_queue(&Q, 4);
+    en_queue(&Q, 5);
+    en_queue(&Q, 6);
+    en_queue(&Q, 7);
+    en_queue(&Q, 8);
+    traverse_queue(&Q);
 
+    if (out_queue(&Q, &Val))
+    {
+        printf("出队成功, %d\n", Val);
+    }
+
+    else
+    {
+        printf("失败\n");
+    }
+    traverse_queue(&Q);
     return 0;
 }
