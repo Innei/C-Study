1e146e91b7aa919e65490cbba64e56dacd9ee9e8
yiny0liuyin <yi00oi9hhg@qq.com>
2019-01-24 16:16:26 +0800

    cleanup

--- 数据结构/队列queue.c
+++ 数据结构/队列queue.c
@@ -9,15 +9,39 @@ typedef struct Queue
     int rear;
 } QUEUE;
 
-void init(QUEUE *Q)
+void init(QUEUE *pQ)
+{
+    pQ->pBase = (int *)malloc(sizeof(int) * 6);
+    pQ->front = 0;
+    pQ->rear = 0;
+}
+bool full_queue(QUEUE *pQ)
 {
 
+    if ((pQ->rear + 1) % 6 == pQ->front)
+    {
+        return true;
+    }
+    return false;
 }
-
+bool en_queue(QUEUE *pQ, int val)
+{
+    if (full_queue(pQ))
+        return false;
+    pQ->pBase[pQ->rear] = val;
+    pQ->rear = (pQ->rear + 1) % 6; // 长度
+    return true;
+}
+void traverse_queue(QUEUE *pQ)
+{
+    
+}
 
 int main(int argc, char const *argv[])
 {
     QUEUE Q;
+
+    init(&Q);
 
     return 0;
 }
